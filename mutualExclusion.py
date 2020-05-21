9d9fd4e3b0527ce06350813096c168e8557e76e0
Joan Merce Lopez <joan.merce@estudiants.urv.cat>
2020-05-21 21:34:25 +0200

    Final v1

--- mutualExclusion.py
+++ mutualExclusion.py
@@ -10,26 +10,30 @@ N_SLAVES = 10
 def master(id, x, ibm_cos):
     write_permission_list = []
     
-    # 1. monitor COS bucket each X seconds
     objects = ibm_cos.list_objects(Bucket=bucket)['Contents']
     obj2 = list()
     for obj in objects:
-        if "p_write_" in obj['Key']:        # 2. List all "p_write_{id}" files
+        if "p_write_" in obj['Key']:        # List all "p_write_{id}" files
             obj2.append(obj)
     
-    while len(obj2) > 0:    
-
-    # 3. Order objects by time of creation
-        #ordenat = sorted(obj2['LastModified'])
+    while len(obj2) > 0:                    # until no "p_write_{id}" objects in the bucket
+        obj2.sort(key=lambda k: k['LastModified'], reverse=True)      # Order objects by time of creation
     
-    # 4. Pop first object of the list "p_write_{id}"
+        elem = obj2.pop(0)                                              # Pop first object of the list "p_write_{id}"
+        idElem = elem['Key'][9:]
 
-    # 5. Write empty "write_{id}" object into COS
-    # 6. Delete from COS "p_write_{id}", save {id} in write_permission_list
+        f = open("write_"+idElem, "wb+")                                # Write empty "write_{id}" object into COS
+        ibm_cos.put_object(Bucket=bucket, Key="write_"+idElem, Body=f)
+
+        ibm_cos.delete_object(Bucket=bucket, Key="p_write_"+idElem)     # Delete from COS "p_write_{id}"
+        write_permission_list.append(idElem)                            # save {id} in write_permission_list
+
     # 7. Monitor "result.json" object each X seconds until it is updated
-    # 8. Delete from COS “write_{id}”
-    # 8. Back to step 1 until no "p_write_{id}" objects in the bucket
-        #time.sleep(x)
+        #json = ibm_cos.get_object(Bucket=bucket, Key='result.json')['Body'].read()
+
+        ibm_cos.delete_object(Bucket=bucket, Key="write_"+idElem)       # Delete from COS “write_{id}”
+        
+        time.sleep(x)                                                   # monitor COS bucket each X seconds
         objects = ibm_cos.list_objects(Bucket=bucket)['Contents']
         obj2 = list()
         for obj in objects:
@@ -40,36 +44,46 @@ def master(id, x, ibm_cos):
 
 
 def slave(id, x, ibm_cos):
-    # 1. Write empty "p_write_{id}" object into COS
-    f = open("p_write_"+str(id), "w")
-    fSer = pickle.dumps(f)
-    ibm_cos.put_object(Bucket=bucket, Key="p_write_"+str(id), Body=fSer)
+    # Write empty "p_write_{id}" object into COS
+    f = open("p_write_"+str(id), "wb+")
+    ibm_cos.put_object(Bucket=bucket, Key="p_write_"+str(id), Body=f)
 
-    # 2. Monitor COS bucket each X seconds until it finds a file called "write_{id}"
-    objects = ibm_cos.list_objects(Bucket=bucket)['Contents']['Key']
-    while ("write_"+str(id) not in objects):
-        objects = ibm_cos.list_objects(Bucket=bucket)['Contents']['Key']
+    # Monitor COS bucket each X seconds until it finds a file called "write_{id}"
+    objects = ibm_cos.list_objects(Bucket=bucket)['Contents']
+    continuar = True
+    for obj in objects:
+        if "write_"+str(id) in obj['Key']: continuar = False
+    while (continuar):
+        objects = ibm_cos.list_objects(Bucket=bucket)['Contents']
+        continuar = True
+        for obj in objects:
+            if "write_"+str(id) in obj['Key']: continuar = False
         time.sleep(x)
     
-    # 3. If write_{id} is in COS: get result.txt, append {id}, and put back to COS result.txt
-    resultSer = ibm_cos.get_object(Bucket=bucket, Key='result.txt')['Body'].read()
+    # If write_{id} is in COS: get result.txt, append {id}, and put back to COS result.txt
+    resultSer = ibm_cos.get_object(Bucket=bucket, Key='result.txt')['Body'].read() 
     result = pickle.loads(resultSer)
-    result.append(str(id))
+    result.append(str(id)+" ")
     resultSer = pickle.dumps(result)
     ibm_cos.put_object(Bucket=bucket, Key="result.txt", Body=resultSer)
 
-    # 4. Finish
-    # No need to return anything
 
 
 if __name__ == '__main__':
 
     pw = pywren.ibm_cf_executor()
     pw.call_async(master, 0)
-    #pw.map(slave, range(N_SLAVES))
+    pw.map(slave, range(N_SLAVES))
     write_permission_list = pw.get_result()
     
-    print(write_permission_list)
-    # Get result.txt
-    #result = ibm_cos.get_object(Bucket=bucket, Key='result.txt')['Body'].read()
+    ibm_cos = pw.internal_storage.get_client()
+    result = ibm_cos.get_object(Bucket=bucket, Key='result.txt')['Body'].read()         # Get result.txt
+    result = result.decode()
+
     # check if content of result.txt == write_permission_list
+    separat = result.split() 
+    for i in len(separat):
+        print(separat[i]+" "+write_permission_list[i]+"\n")
+        if separat[i] != write_permission_list[i]:
+            print("No s'ha escrit en el mateix ordre.\n")
+
