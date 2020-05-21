276c83c78000a8641b625355f81121447234abdc
Joan Merce Lopez <joan.merce@estudiants.urv.cat>
2020-05-21 18:41:33 +0200

    proves al master

--- mutualExclusion.py
+++ mutualExclusion.py
@@ -10,14 +10,26 @@ N_SLAVES = 10
 def master(id, x, ibm_cos):
     write_permission_list = []
     # 1. monitor COS bucket each X seconds
+    objects = ibm_cos.list_objects(bucket_name=bucket)['Contents']['Key']
+    for nom in objects:
+        if "p_write_" in nom:
+            print("hola")
+
     # 2. List all "p_write_{id}" files
+
+
     # 3. Order objects by time of creation
+
+
     # 4. Pop first object of the list "p_write_{id}"
     # 5. Write empty "write_{id}" object into COS
     # 6. Delete from COS "p_write_{id}", save {id} in write_permission_list
     # 7. Monitor "result.json" object each X seconds until it is updated
     # 8. Delete from COS “write_{id}”
     # 8. Back to step 1 until no "p_write_{id}" objects in the bucket
+        time.sleep(x)
+        objects = ibm_cos.list_objects(bucket_name=bucket)['Contents']['Key']
+
     return write_permission_list
 
 
@@ -28,9 +40,9 @@ def slave(id, x, ibm_cos):
     ibm_cos.put_object(Bucket=bucket, Key="p_write_"+str(id), Body=fSer)
 
     # 2. Monitor COS bucket each X seconds until it finds a file called "write_{id}"
-    objects = ibm_cos.list_objects(bucket_name=bucket)
+    objects = ibm_cos.list_objects(bucket_name=bucket)['Contents']['Key']
     while ("write_"+str(id) not in objects):
-        objects = ibm_cos.list_objects(bucket_name=bucket)
+        objects = ibm_cos.list_objects(bucket_name=bucket)['Contents']['Key']
         time.sleep(x)
     
     # 3. If write_{id} is in COS: get result.txt, append {id}, and put back to COS result.txt
@@ -39,7 +51,7 @@ def slave(id, x, ibm_cos):
     result.append(str(id))
     resultSer = pickle.dumps(result)
     ibm_cos.put_object(Bucket=bucket, Key="result.txt", Body=resultSer)
-    
+
     # 4. Finish
     # No need to return anything
 
